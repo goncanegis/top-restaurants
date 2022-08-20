90d49b7046855db5bf4e8ffd8287dc409051eee8
Gonca Negis <gonca.negis@gmail.com>
2022-08-20 08:36:39 +0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Nav />
+
+    <div class="container text-center">
+      <h1>Welcome to Restaurantly</h1>
+      <a href="/restaurants">Go to restaurants</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  margin-top: 5rem;
+}
+</style>
